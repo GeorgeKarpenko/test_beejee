77431ef7794013ecb7ff66e49a4c7aeddb8b43ec
GeorgeKarpenko <mynameisgeorgekarpenko@yandex.ru>
2020-10-12 00:58:02 +0300

    master

--- view/errors/403.php
+++ view/errors/403.php
@@ -4,7 +4,7 @@ use App\Modules\HTML;
 
 // окончательный HTML код
 $layout_content = HTML::phptpl('layouts/app',
-['content' => '', 'title' => '403']);
+['content' => '<a href="/">Главная страница</a>', 'title' => '403']);
 
 // вывод на экран итоговой страницы
 print($layout_content);
--- view/errors/404.php
+++ view/errors/404.php
@@ -4,7 +4,7 @@ use App\Modules\HTML;
 
 // окончательный HTML код
 $layout_content = HTML::phptpl('layouts/app',
-['content' => '', 'title' => '404']);
+['content' => '<a href="/">Главная страница</a>', 'title' => '404']);
 
 // вывод на экран итоговой страницы
 print($layout_content);
